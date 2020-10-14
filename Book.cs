7475b4ff556086cfaca4d4e39385feae2abcac1e
William <william.marback@hotmail.com>
2020-10-14 10:37:10 +0200

    Gjorde PrintInfo-metoden, skriver ut bokens info

--- Book.cs
+++ Book.cs
@@ -4,6 +4,7 @@ namespace Prov1
 {
     public class Book
     {
+        //Alla varibler samt strings
         public int price;
         private string name;
         private int rarity;
@@ -12,10 +13,12 @@ namespace Prov1
         private bool cursed;
         private Random generator;
 
+        //Konstruktorn som gör huvuddel av programmet, tillkallar andra metoder, etc.
         public Book()
         {
-            rarity = generator.Next(100);
-            actualValue = generator.Next(100);
+            //Genererar ett "random" värde för bokens värde samt raritet
+            rarity = generator.Next(0, 100);
+            actualValue = generator.Next(0, 100);
 
             //Gör det 50% att det kan bli varken true eller false
             if (generator.Next(2) == 0)
@@ -23,10 +26,14 @@ namespace Prov1
                 cursed = true;
             }
         }
-
+        
+        //Skriver ut bokes info
         public void PrintInfo()
         {
-
+            System.Console.WriteLine("The books name is: " + name);
+            System.Console.WriteLine("\nThe books rarity is: " + rarity);
+            System.Console.WriteLine("\nThe books category is: " + category);
+            System.Console.WriteLine("\n\nThe books final price is: " + price);
         }
 
         public int Evaluate()
